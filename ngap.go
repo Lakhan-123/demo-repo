c24fcad5f43b7f7ab0e8e563dcf44ba7a3a51a1a
Lakhan <lakhan.ghayade@amantyatech.com>
2022-03-27 15:25:41 +0530

    m2

--- ngap.go
+++ ngap.go
@@ -3,5 +3,6 @@ Package main
 import "fmt"
 
 func main() { 
-  fmt.Println("This file is created and written on github")
+	fmt.Println("This file is created and written on github")
+	fmt.Println("I edited this file in symtems vim") 
 }
